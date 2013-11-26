0481c802d1313a680c114788cd70709b192f9a71
Ryan Ringler <rringler@gmail.com>
2013-11-26 10:35:22 -0800

    Updates views to check if a user is logged in before showing user specific items.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,5 +13,4 @@ class Vote < ActiveRecord::Base
 
   # Comment out the line below to allow multiple votes per user.
   validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
-
 end
