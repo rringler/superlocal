037f1061d6578da94d526a410049064a0253ebab
Ryan Ringler <rringler@gmail.com>
2013-12-10 19:41:14 -0800

    Fixes Address model initializer default

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address
 	attr_reader :address2, :city, :state, :zip5
 
-	def initialize(args=nil)
+	def initialize(args={})
 		@address2 = args[:address2] || nil
 		@city     = args[:city]     || nil
 		@state    = args[:state]    || nil
