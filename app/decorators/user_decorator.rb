1e3871b8091c8b4eb5526e36dae81d1334940790
Ryan Ringler <rringler@gmail.com>
2013-12-16 20:34:13 -0800

    Removes UserDecorator#following_board override.  Acts_as_follower v0.2.1 now correctly implements #responds_to? for dynamic methods.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,10 +8,4 @@ class UserDecorator < Draper::Decorator
   def recent_comments
     object.comments.recent
   end
-
-  # Draper appears to have some issue delegating dynamic methods to the
-  # decorated object.
-  def following_boards
-    object.following_boards
-  end
 end
